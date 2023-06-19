2775232e6ca4f17ed8af44a773a47c2ad93f1f08
@hexlet/cli <support@hexlet.io>
2023-06-19 21:02:47 +0500

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 // BEGIN
 public class App {
+    public static String[] duplicateValues(String[] items) {
+        return Arrays.stream(items)
+                .flatMap(item -> Arrays.stream(new String[]{item, item}))
+                .toArray(String[]::new);
+    }
     public static String[][] enlargeArrayImage(String[][] image) {
-        String[][] enlargedImg = new String[image.length * 2][image.length * 2];
-        for (int i = 0; i < enlargedImg.length; i++) {
-            for (int j = 0; j < enlargedImg.length; j++){
-                if (j == 0 || j == 1 || j == enlargedImg.length - 2 || j == enlargedImg.length - 1 || i == 0 || i == 1 || i == enlargedImg.length - 1 || i == enlargedImg.length - 2) {
-                    enlargedImg[i][j] = "*";
-                } else {
-                    enlargedImg[i][j] = " ";
-                }
-            }
-        }
-        return enlargedImg;
+        String[][] horizontalyStretched = Arrays.stream(image)
+                .map(App::duplicateValues)
+                .toArray(String[][]::new);
+        return Arrays.stream(horizontalyStretched)
+                .flatMap(item -> Arrays.stream(new String[][]{item, item}))
+                .toArray(String[][]::new);
     }
 }
 // END
